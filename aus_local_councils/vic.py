8493b4ea4bfa1b309e62e4273485afd75198a114
king-millez <Millez.Dev@gmail.com>
2020-12-10 20:16:38 +1100

    [vic] get all Victorian councils

--- aus_local_councils/vic.py
+++ aus_local_councils/vic.py
@@ -1,13 +1,14 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-url = 'https://www.viccouncils.asn.au/find-your-council/council-contacts-list'
+url = 'https://knowyourcouncil.vic.gov.au/councils'
 
 def get_councils():
     _council_names = []
-    rows = BeautifulSoup(get(url).text, 'lxml').find('table', {'id': 'table19340'}).find_all('tr')
-    for row in rows:
-        _council_names.append(row.find('p').text)
-    return(_council_names)
+    div = BeautifulSoup(get(url).text, 'lxml')
+    councils = div.find_all('li')
+    for council in councils[9:]:
+        _council_names.append(council.text)
+    return _council_names
 
 councils = get_councils()
