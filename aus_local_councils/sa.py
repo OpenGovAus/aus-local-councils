eab7c62e8ccc6402283424432f5d10b6d997f635
king-millez <Millez.Dev@gmail.com>
2020-12-10 23:16:21 +1100

    [sa] use ECSA to get LGAs without communities

--- aus_local_councils/sa.py
+++ aus_local_councils/sa.py
@@ -1,13 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-url = 'https://www.lga.sa.gov.au/sa-councils/councils-listing'
+url = 'https://www.ecsa.sa.gov.au/council-links'
 
 def get_councils():
     council_list = []
-    _list = BeautifulSoup(get(url).text, 'lxml').find('div', id='content_container_538648')
-    for entry in _list.find_all('li', {'class': 'sacouncils-listing-item'}):
-        council_list.append(entry.text.strip())
-    return council_list
+    _list = BeautifulSoup(get(url).text, 'lxml').find('div', {'class': 'content-text'}).find_all('li')
+    for council in _list:
+        council_list.append(council.text.replace('\u00a0', ' '))
+    return(council_list)
 
 councils = get_councils()
